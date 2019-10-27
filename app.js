90f8e1ae26fe09c7a199c2f7bfe9ba8cf30dcbfb
Olivier benson-smith <olliebenson@hotmail.co.uk>
2019-10-27 14:02:01 +0000

    new variables added displaying information on webpage

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 window.addEventListener('load', () => {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimezone = document.querySelector('.location-timezone');
+
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
+      const proxy = 'https://cors-anywhere.herokuapp.com/';
 
-      const api = `https://api.darksky.net/forecast/d8506f2317738a48a1b0ea93b90bae47/${lat},${long}`;
+      const api = `${proxy}https://api.darksky.net/forecast/d8506f2317738a48a1b0ea93b90bae47/${lat},${long}`;
 
-      fetch(api).then(response => {
-        return response.json();
-      }).then(data => {
-        console.log(data)
-      })
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const { temperature, summary } = data.currently;
+
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
+        });
     });
 
   }
