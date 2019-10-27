7e11e0dee25ba09495a1b43d2b83d8460663846b
Olivier benson-smith <olliebenson@hotmail.co.uk>
2019-10-27 13:07:20 +0000

    long and lat variables created and api inserted

---
+++ app.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const api = `https://api.darksky.net/forecast/d8506f2317738a48a1b0ea93b90bae47/${lat},${long}`
+    });
+  }
+  else {
+    h1.textContent = "Please allow location services for this page.";
+  }
+});
