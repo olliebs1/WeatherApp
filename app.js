8e3dd77748bba098ac2db213a4fc97ce4aa3b69c
Olivier benson-smith <olliebenson@hotmail.co.uk>
2019-10-27 13:29:39 +0000

    added fetch function to return data from api

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ window.addEventListener('load', () => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const api = `https://api.darksky.net/forecast/d8506f2317738a48a1b0ea93b90bae47/${lat},${long}`
+
+      const api = `https://api.darksky.net/forecast/d8506f2317738a48a1b0ea93b90bae47/${lat},${long}`;
+
+      fetch(api).then(response => {
+        return response.json();
+      }).then(data => {
+        console.log(data)
+      })
     });
+
   }
   else {
     h1.textContent = "Please allow location services for this page.";
